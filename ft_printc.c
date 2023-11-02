98f9fb0ca92da146d4480ba33c22400fe0b77e92
yuurin98 <119097638+yuurin98@users.noreply.github.com>
2023-11-02 09:24:31 +0000

    Created print c

---
+++ ft_printc.c
@@ -0,0 +1,4 @@
+#include "ft_printf.h"
+#include <stdarg.h>
+
+int 
--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lchee-ti <lchee-ti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:20:46 by lchee-ti          #+#    #+#             */
-/*   Updated: 2023/11/02 13:56:42 by lchee-ti         ###   ########.fr       */
+/*   Updated: 2023/11/02 08:57:09 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,19 @@ static int	ft_format(char specifier, va_list args)
 	else if (specifier == 's')
 		count += ft_putstr_fd(va_arg(args, char *), 1);
 	else if (specifier == 'p')
+		count +=
 	else if (specifier == 'd')
 		count += ft_putnbr_fd(va_arg(args, int), 1);
 	else if (specifier == 'i')
+		count +=
 	else if (specifier == 'u')
+		count +=
 	else if (specifier == 'x')
+		count +=
 	else if (specifier == 'X')
+		count +=
 	else if (specifier == '%')
+		count +=
 	else
 		count += write(1, &specifier, 1);
 	return (count);
